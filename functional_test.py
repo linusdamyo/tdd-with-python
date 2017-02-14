c63612bc00d9ffbdd25d8ee487af1ca053ad9e60
linusdamyo <linusdamyo@gmail.com>
2017-02-14 23:23:50 +0900

    기능테스트 변경내용 적용

--- functional_test.py
+++ functional_test.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 웹사이트 접근
         self.browser.get('http://localhost:8000')
@@ -35,6 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         # 엔터키를 치면 페이지가 갱신되고 작업목록에
         # 1: 공작깃털 사기 아이템이 추가된다.
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         # 추가 아이템을 입력할 여분의 텍스트 상자가 보인다.
         # 다시 공작깃털 이용해서 그물 만들기 입력
@@ -43,11 +49,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 페이지가 다시 갱신되고, 두개 아이템이 목록에 보인다.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
-        self.assertIn('2: 공작깃털을 이용해서 그물 만들기', [row.text for row in rows])
-
+        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         # 사이트는 특정 url 을 생성한다.
         # 이때 url에 대한 설명도 함께 제공된다.
