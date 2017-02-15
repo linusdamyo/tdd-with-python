b67f67f56368546b54b306e616eb65259b519e3c
linusdamyo <linusdamyo@gmail.com>
2017-02-16 00:39:45 +0900

    모델을 조정해서 아이템들이 다른 목록과 연계되도록 한다.

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -19,16 +19,23 @@ class HomePageTest(TestCase):
 		self.assertEqual(response.content.decode(), expected_html)
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
 		first_item = Item()
 		first_item.text = '첫번째 아이템'
+		first_item.list = list_
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = '두번째 아이템'
+		second_item.list = list_
 		second_item.save()
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
 
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -36,7 +43,9 @@ class ItemModelTest(TestCase):
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, '첫번째 아이템')
+		self.assertEqual(first_saved_item.list, list_)
 		self.assertEqual(second_saved_item.text, '두번째 아이템')
+		self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -46,8 +55,9 @@ class ListViewTest(TestCase):
 		self.assertTemplateUsed(response, 'list.html')
 
 	def test_displays_all_items(self):
-		Item.objects.create(text='첫번째 아이템')
-		Item.objects.create(text='두번째 아이템')
+		list_ = List.objects.create()
+		Item.objects.create(text='첫번째 아이템', list=list_)
+		Item.objects.create(text='두번째 아이템', list=list_)
 
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 
