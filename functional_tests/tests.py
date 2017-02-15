a01637fa275be02a48c049434f8ca608d724fd63
linusdamyo <linusdamyo@gmail.com>
2017-02-15 23:29:09 +0900

    새로운 유저 프란시스에 대한 기능테스트를 추가했다.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -40,6 +40,8 @@ class NewVisitorTest(LiveServerTestCase):
         # 엔터키를 치면 페이지가 갱신되고 작업목록에
         # 1: 공작깃털 사기 아이템이 추가된다.
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         # 추가 아이템을 입력할 여분의 텍스트 상자가 보인다.
@@ -52,8 +54,32 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
         self.check_for_row_in_list_table('1: 공작깃털 사기')
 
-        # 사이트는 특정 url 을 생성한다.
-        # 이때 url에 대한 설명도 함께 제공된다.
-        self.fail('Finish the test!')
+        # 새로운 사용자인 프란시스가 사이트에 접속한다.
 
-        # 해당 url 에 접속하면, 작업 목록이 그대로 나온다.
+        ## 새로운 브라우저 세션을 이용해서 에디스의 정보가
+        ## 쿠키를 통해 유입되는 것을 방지한다.
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # 프란시스가 홈페이지에 접속한다.
+        # 에디스의 리스트는 보이지 않는다.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작깃털 사기', page_text)
+        self.assertNotIn('그물 만들기', page_text)
+
+        # 프란시스가 새로운 작업 아이템을 입력하기 시작한다.
+        # 그는 에디스보다 재미가 없다.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('우유 사기')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 프란시스가 전용 URL을 취득한다.
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 에디스가 입력한 흔적이 없다는 것을 다시 확인한다.
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작깃털 사기', page_text)
+        self.assertIn('우유 사기', page_text)
