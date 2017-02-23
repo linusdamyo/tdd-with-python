c7b8f02ce7a01d8c15caae721a367f0d3a404484
linusdamyo <linusdamyo@gmail.com>
2017-02-23 14:54:28 +0900

    단위테스트를 두개파일로 나누기

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -19,35 +19,6 @@ class HomePageTest(TestCase):
 		self.assertEqual(response.content.decode(), expected_html)
 
 
-class ListAndItemModelTest(TestCase):
-
-	def test_saving_and_retrieving_items(self):
-		list_ = List()
-		list_.save()
-
-		first_item = Item()
-		first_item.text = '첫번째 아이템'
-		first_item.list = list_
-		first_item.save()
-
-		second_item = Item()
-		second_item.text = '두번째 아이템'
-		second_item.list = list_
-		second_item.save()
-		saved_list = List.objects.first()
-		self.assertEqual(saved_list, list_)
-
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
-
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-		self.assertEqual(first_saved_item.text, '첫번째 아이템')
-		self.assertEqual(first_saved_item.list, list_)
-		self.assertEqual(second_saved_item.text, '두번째 아이템')
-		self.assertEqual(second_saved_item.list, list_)
-
-
 class ListViewTest(TestCase):
 
 	def test_uses_list_template(self):
@@ -113,5 +84,3 @@ class NewItemTest(TestCase):
 		correct_list = List.objects.create()
 		response = self.client.get('/lists/%d/' % (correct_list.id,))
 		self.assertEqual(response.context['list'], correct_list)
-
-		
