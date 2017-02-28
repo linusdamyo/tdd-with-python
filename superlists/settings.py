584dab4e6d913b26ac0b24d4cb73d4ae6ec73935
linusdamyo <linusdamyo@gmail.com>
2017-02-28 19:30:06 +0900

    사용자 정의 퍼소나 백엔드 인증 + 사용자 정의 user 모델

--- superlists/settings.py
+++ superlists/settings.py
@@ -54,6 +54,10 @@ ROOT_URLCONF = 'superlists.urls'
 
 WSGI_APPLICATION = 'superlists.wsgi.application'
 
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'accounts.authentication.PersonaAuthenticationBackend'
+)
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
